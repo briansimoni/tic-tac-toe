a25934fdf163153e67b4717c2de8874f87623ef4
Brian Simoni <simonibc@vcu.edu>
2017-06-14 19:05:29 -0400

    Add printGameBoard

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -4,4 +4,23 @@ package com.company;
  * Created by brian on 6/14/17.
  */
 public class Board {
+	int[][] gameBoard;
+
+	Board() {
+		this.gameBoard = new int[3][3];
+	}
+
+	int[][] getGameBoard() {
+		return this.gameBoard;
+	}
+
+	void printGameBoard() {
+		for(int i = 0; i < this.gameBoard.length; i++ ) {
+			System.out.println("\n");
+			for(int j = 0; j < this.gameBoard.length; j++ ) {
+				System.out.print(this.gameBoard[i][j]);
+				System.out.print(" ");
+			}
+		}
+	}
 }
